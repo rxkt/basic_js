07dec68dddc40b3a27c8b6e6a9e7e688051cf324
CallMeSenpai <ericliao10797@hotmail.com>
2014-12-02 01:27:11 -0500

    :P

--- actions.js
+++ actions.js
@@ -5,23 +5,49 @@ var random_num = function(x){//returns random from 0 to x
 }
 
 var list_of_circles = [];
-
+var threshold =0;
 setInterval(timer,500);
+
 function timer(){   
-    var hitcircle= {
-	x:random_num(600)+100,
-	y:random_num(400)+100,
-	id:"hitcircle",
-	approach_circle_size:70,//default?
-	approach_filename:"approachcircle.png",
-	overlay_id:"hitcircleoverlay"
-    }
-    list_of_circles.push(hitcircle);
-    var c=document.getElementById("myCanvas");
+    var c=document.getElementById("canvas");
     var ctx=c.getContext("2d");
-    var img=document.getElementById(hitcircle.id);
-    var img2=document.getElementById(hitcircle.overlay_id);
-    ctx.drawImage(img,hitcircle.x,hitcircle.y);
-    ctx.drawImage(img2,hitcircle.x,hitcircle.y);
-    console.log(hitcircle);
+    threshold=(threshold+1)%2;
+    //console.log(threshold);
+    if (threshold>0){
+	//hitcircle is inside timer() to ensure randomness.
+	var hitcircle= {
+	    x:random_num(600)+50,
+	    y:random_num(400)+50,
+	    id:"hitcircle",
+	    a_c_size:70,//approach circle size.-not yet implemented
+	    //approach_filename:"approachcircle.png",
+	    overlay_id:"hitcircleoverlay",
+	    hit0:"0",
+	    hit100:"100",
+	    wait:0
+	}
+	list_of_circles.push(hitcircle);
+	for (circle in list_of_circles){
+	    circle.wait++;
+	    
+	}
+	c.addEventListener('click',
+			   function(e){
+			       console.log(e);
+			       console.log(this);
+			   });
+	var bg=document.getElementById("bg");
+	ctx.drawImage(bg,0,0);	
+	var img=document.getElementById(hitcircle.id);
+	var img2=document.getElementById(hitcircle.overlay_id);
+	ctx.drawImage(img,hitcircle.x,hitcircle.y);
+	ctx.drawImage(img2,hitcircle.x,hitcircle.y);
+    }
+    //loop through list_of_circles, if 1 second passes, gg.
+    //0.5 to appear, 0.5 hitframe.
+    //gg = remove circle.
+    //console.log(hitcircle); no more debug sir
 }
+
+    
+/////////////////////////////////////////////legit stuff
