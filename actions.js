d0eecc64acdb23d6fe5dee8a39ab30c38f979f99
CallMeSenpai <ericliao10797@hotmail.com>
2014-12-02 00:01:59 -0500

    circles appear every .5 sec

--- actions.js
+++ actions.js
@@ -9,17 +9,19 @@ var list_of_circles = [];
 setInterval(timer,500);
 function timer(){   
     var hitcircle= {
-	x:random_num(800),
-	y:random_num(600),
-	filename:"hitcircle.png",
+	x:random_num(600)+100,
+	y:random_num(400)+100,
+	id:"hitcircle",
 	approach_circle_size:70,//default?
 	approach_filename:"approachcircle.png",
-	overlay_filename:"hitcircleoverlay.png"
+	overlay_id:"hitcircleoverlay"
     }
     list_of_circles.push(hitcircle);
     var c=document.getElementById("myCanvas");
     var ctx=c.getContext("2d");
-    var img=document.getElementById("hitcircle");
+    var img=document.getElementById(hitcircle.id);
+    var img2=document.getElementById(hitcircle.overlay_id);
     ctx.drawImage(img,hitcircle.x,hitcircle.y);
+    ctx.drawImage(img2,hitcircle.x,hitcircle.y);
     console.log(hitcircle);
 }
